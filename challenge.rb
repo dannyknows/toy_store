e0480e8cc15b3110d1a2cdf94c362e21e354edc6
dannyknows <dannylow465@gmail.com>
2020-04-16 15:41:12 +1000

    intial commit

---
+++ challenge.rb
@@ -0,0 +1,35 @@
+# To hook up our MVC knowledge, we will create a Toy Store!
+
+# Core:
+# * Create a new rails app called 'toy_store', using postgresql as our db
+# * Change into this directory and push our initial app to github
+# * Create our database
+
+# * Using 'rails g migration' - Create a table in our database, called 'toy' that will hold:
+#     * A name - string
+#     * A description - text
+#     * A picture - text
+#     * A date the item was posted - date
+#     * A user - string (will just be someone's name that posted the toy)
+# * Is that the only command we have to run? Nope. rails db:migrate
+# * Oops, we haven't learnt images yet, so lets generate a new migration to solve this
+#   (Hint: Google 'rails 5 remove a column from database')
+# * Remember to migrate our new changes!
+
+# * Create the model to go with this table
+#   (Remember rails naming convention)
+
+# * Jump into rails console, and have a play around, creating new toys and looking them up
+
+# * Setup our seeds file, so that we can create 20 toys
+#   (may want to use faker to get interesting data)
+# * Run the seeds file, so we have at least 20 toys
+
+# Advanced:
+# * We now have our toys, but have no way to interact with them
+# * Add in the necessary views / routes / controllers to make 'toy' a CRUD resource
+#   (This includes adding a form!!!)
+# * Check that we are able to add new toys, and that they remain, even after restarting the server!
+# * Yay! Our first complete MVC application
+
+# * Push those changes up to github!
